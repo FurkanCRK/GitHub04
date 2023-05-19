72572461ed7c65d31b0d78dc7cf47cfa18ecbd5c
FurkanCRK <fcyazilim35@gmail.com>
2023-05-19 22:34:52 +0300

    4.gun

---
+++ src/G02.java
@@ -0,0 +1,23 @@
+public class G02 {
+
+    /*
+        Git- Github'da projelerimizin üzerinde calisirken temelde 3 adimda islem yapiyoruz
+
+        1- Projelerimizi/Dosyalarimizi git ile iliskilendiriyoruz
+
+                git init
+
+        2- projemizde/dosyamizda bulunanlari versiyon olusturuyorduk
+
+                git add .
+                git commit -m"versiyonadi"
+
+        3- github'a yollama/entegrasyonu
+
+                git remote add origin repoUrlAderesi
+                already origin
+                git remote set-url repoAdresi
+
+                git push
+     */
+}
